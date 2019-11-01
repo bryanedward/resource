c1f964f175b04d5c670db0a999f373377b4e9af4
bryanedward <46612406+bryanedward@users.noreply.github.com>
2019-10-31 21:29:51 -0500

    script

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 module.exports = {
   port: process.env.PORT || 5432,
-  SECRET_TOKEN : 'KSFSDKFEOWKR',
-  DB_CONNECT : 'mongodb+srv://edwardbrian96:<ryanescor96>@devcode-6dkh6.mongodb.net/test?retryWrites=true&w=majority'
+  SECRET_TOKEN : 'KSFSDKFEOWKR'
 };
